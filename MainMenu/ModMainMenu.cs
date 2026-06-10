14ff7e2b476f54c05a3f49ba546d1372bdb94098
Dycel <168293407+Dycellll@users.noreply.github.com>
2026-06-10 22:22:25 +0200

    Update ModMainMenu.cs
    Small fix of the main menu classes not having a namespace assigned

--- MainMenu/ModMainMenu.cs
+++ MainMenu/ModMainMenu.cs
@@ -4,45 +4,48 @@ using ReLogic.Content;
 using Terraria;
 using Terraria.ModLoader;
 
-public class MainMenuBackground : ModSurfaceBackgroundStyle
+namespace TerrariaTenebrous.MainMenu
 {
-    public override void ModifyFarFades(float[] fades, float transitionSpeed) { }
-
-    public override bool PreDrawCloseBackground(SpriteBatch spriteBatch)
+    public class MainMenuBackground : ModSurfaceBackgroundStyle
     {
-        Texture2D bg = ModContent.Request<Texture2D>(
-            $"{nameof(TerrariaTenebrous)}/MainMenu/Background",
-            AssetRequestMode.ImmediateLoad
-        ).Value;
+        public override void ModifyFarFades(float[] fades, float transitionSpeed) { }
 
-        spriteBatch.Draw(
-            bg,
-            new Rectangle(0, 0, Main.screenWidth, Main.screenHeight),
-            Color.White
-        );
+        public override bool PreDrawCloseBackground(SpriteBatch spriteBatch)
+        {
+            Texture2D bg = ModContent.Request<Texture2D>(
+                $"{nameof(TerrariaTenebrous)}/MainMenu/Background",
+                AssetRequestMode.ImmediateLoad
+            ).Value;
+
+            spriteBatch.Draw(
+                bg,
+                new Rectangle(0, 0, Main.screenWidth, Main.screenHeight),
+                Color.White
+            );
 
 
-        return true;
+            return true;
+        }
     }
-}
 
-public class ModMainMenu : ModMenu
-{
-    public override string DisplayName => "Tenebrous Mod";
-
-    public override int Music =>
-        MusicLoader.GetMusicSlot(Mod, "Assets/Music/TitleTheme");
-
-    public override Asset<Texture2D> Logo =>
-        ModContent.Request<Texture2D>(
-            $"{nameof(TerrariaTenebrous)}/MainMenu/ModLogo"
-        );
-
-    public override ModSurfaceBackgroundStyle MenuBackgroundStyle =>
-        ModContent.GetInstance<MainMenuBackground>();
-
-    public override void Load()
+    public class ModMainMenu : ModMenu
     {
-        Main.menuBGChangedDay = true;
+        public override string DisplayName => "Tenebrous Mod";
+
+        public override int Music =>
+            MusicLoader.GetMusicSlot(Mod, "Assets/Music/TitleTheme");
+
+        public override Asset<Texture2D> Logo =>
+            ModContent.Request<Texture2D>(
+                $"{nameof(TerrariaTenebrous)}/MainMenu/ModLogo"
+            );
+
+        public override ModSurfaceBackgroundStyle MenuBackgroundStyle =>
+            ModContent.GetInstance<MainMenuBackground>();
+
+        public override void Load()
+        {
+            Main.menuBGChangedDay = true;
+        }
     }
 }
